c2fce8cb16f29936637b66b8bf1b9943a73659c8
Ryo Nakano <ryo.z.nakano@gmail.com>
2021-01-17 14:35:59 +0900

    Add Gigasecond at Ruby Track

---
+++ ruby/gigasecond/gigasecond.rb
@@ -0,0 +1,12 @@
+=begin
+Write your code for the 'Gigasecond' exercise in this file. Make the tests in
+`gigasecond_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/gigasecond` directory.
+=end
+class Gigasecond
+  def self.from(time)
+    time + 1_000_000_000
+  end
+end
