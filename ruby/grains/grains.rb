6ce00fc32bbd24efa9bae24139202184c277c03f
Ryo Nakano <ryo.z.nakano@gmail.com>
2021-01-17 14:44:57 +0900

    Add Grains at Ruby Track

---
+++ ruby/grains/grains.rb
@@ -0,0 +1,17 @@
+=begin
+Write your code for the 'Grains' exercise in this file. Make the tests in
+`grains_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/grains` directory.
+=end
+class Grains
+  def self.square(number)
+    raise ArgumentError if number <= 0 || number > 64
+    2 ** (number - 1)
+  end
+
+  def self.total
+    (1..64).map { square(_1) }.sum
+  end
+end
