6fc3bdc826299f56d622baa6ed4156d1644e8cc4
Ryo Nakano <ryo.z.nakano@gmail.com>
2021-01-18 12:47:49 +0900

    Add RNA Transcription at Ruby Track

---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,10 @@
+# Write your code for the 'Rna Transcription' exercise in this file. Make the tests in
+# `rna_transcription_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/rna-transcription` directory.
+class Complement
+  def self.of_dna(dna)
+    dna.tr('CGTA', 'GCAU')
+  end
+end
