0cd023f8cf0ceddbbe925e564c1e168974049b3d
Ryo Nakano <ryo.z.nakano@gmail.com>
2021-01-19 13:16:13 +0900

    Add Roman Numerals at Ruby Track

---
+++ ruby/roman-numerals/roman_numerals.rb
@@ -0,0 +1,12 @@
+class Integer
+  ROMANS = [
+    ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+    ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
+    ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
+    ['', 'M', 'MM', 'MMM']
+  ].freeze
+
+  def to_roman
+    digits.map.with_index { ROMANS[_2][_1] }.reverse.join
+  end
+end
